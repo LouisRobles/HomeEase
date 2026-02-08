8e43e0ba26d1638e406dc64c7d03654923b47a4c
Gabstah <renrelleja6@gmail.com>
2026-02-08 21:17:33 +0800

    User UI

---
+++ app/src/main/java/com/example/homeease/Landing.java
@@ -0,0 +1,43 @@
+package com.example.homeease;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Landing extends AppCompatActivity {
+
+    private Button btn_sign_in;
+    private Button btn_sign_up;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_landing);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_sign_up = findViewById(R.id.btn_sign_up);
+
+        btn_sign_in.setOnClickListener(v -> {
+            Intent intent = new Intent(Landing.this, SignIn.class);
+            startActivity(intent);
+        });
+
+        btn_sign_up.setOnClickListener(v -> {
+            Intent intent = new Intent(Landing.this, SignUp.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/homeease/Splash.java
@@ -0,0 +1,39 @@
+package com.example.homeease;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+
+    private Button btn_splash_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_splash_start = findViewById(R.id.btn_splash_start);
+
+        btn_splash_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Splash.this, Landing.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
